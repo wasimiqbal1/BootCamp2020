44e89cf362e7beab43619dfa1cd79e6d5d7f4fe2
wasimiqbal1 <wasim_iqbal@msn.com>
2020-10-25 14:29:17 +0500

    new app

---
+++ new-react-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Hello World React from Wasim Iqbal < /
+        p > < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
