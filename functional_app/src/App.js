df66fc86efd9cd0ca4806e63c13d26f09301aa2c
wasimiqbal1 <wasim_iqbal@msn.com>
2020-11-02 01:19:59 +0500

    Counter with Day Night Option

--- functional_app/src/App.js
+++ functional_app/src/App.js
@@ -4,19 +4,35 @@ import './App.css';
 
 export default function App() {
 
-    let [count, setCount] = useState(0)
-
+    let [count, setCount] = useState(0);
+    let [isMorning, setMorning] = useState(false)
 
     return ( <
-        div >
+        div className = { `box ${isMorning ? 'dayLight' : ''}` } >
 
         <
-        Message counter = { count } > < /Message> <
+        h1 > Good { isMorning ? 'Morning' : 'Night' } < /h1>
+
+        <
+        Message counter = { count }
+        /> <
         br / >
+
         <
         button onClick = {
             () => setCount(++count)
-        } > Update Counter < /button> < /
-        div >
+        } >
+        Update Counter <
+        /button>
+
+        <
+        button onClick = {
+            () => setMorning(!isMorning)
+        } >
+        Update Day <
+        /button>
+
+        <
+        /div>
     );
 }
